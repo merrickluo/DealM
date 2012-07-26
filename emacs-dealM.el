5b61d8f58bac9eda128deae56dd0a8af1bd5baa0
Richard Wong <chao787@qq.com>
2012-07-26 08:25:26 +0800

    add some const values.

---
+++ emacs-dealM.el
@@ -0,0 +1,29 @@
+;; -*- Emacs-Lisp -*-
+;; Last modified: <2012-07-26 08:24:37 Thursday by richard>
+
+;; Copyright (C) 2012 Richard Wong
+
+;; Author: Richard Wong
+;; Email: chao787@gmail.com
+
+;; Version: 0.1
+;; PUBLIC LICENSE: FreeBSD
+
+;; configure path depend on system.
+
+(defconst emacs-root-path
+  (file-name-directory (buffer-file-name)) "Emacs root settings path (in linux)")
+(defconst plugins-path-r
+  (concat emacs-root-path "plugins/" separator) "Reference path of emacs lisp package")
+(defconst settings-path-r
+  (concat emacs-root-path "settings/") "Personel prefer setting of lisp package")
+(defconst template-path-r
+  (concat emacs-root-path "templates/") "Path for template")))
+
+(setq default-directory emacs-root-path)
+
+
+
+(provide 'emacs-dealM)
+;; emacs-dealM ends here.
+;;;
