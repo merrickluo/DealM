67414dcbff50af52a0ae1fb516d03cc9e20e56fc
Richard Wong <chao787@qq.com>
2012-07-26 08:45:25 +0800

    Fix some bug and add theme settings.

--- emacs-dealM.el
+++ emacs-dealM.el
@@ -1,5 +1,5 @@
 ;; -*- Emacs-Lisp -*-
-;; Last modified: <2012-07-26 08:24:37 Thursday by richard>
+;; Last modified: <2012-07-26 08:44:35 Thursday by richard>
 
 ;; Copyright (C) 2012 Richard Wong
 
@@ -12,16 +12,21 @@
 ;; configure path depend on system.
 
 (defconst emacs-root-path
-  (file-name-directory (buffer-file-name)) "Emacs root settings path (in linux)")
+  (file-name-directory (or load-file-name buffer-file-name)) "Emacs root settings path (in linux)")
 (defconst plugins-path-r
-  (concat emacs-root-path "plugins/" separator) "Reference path of emacs lisp package")
+  (concat emacs-root-path "plugins/") "Reference path of emacs lisp package")
 (defconst settings-path-r
   (concat emacs-root-path "settings/") "Personel prefer setting of lisp package")
 (defconst template-path-r
-  (concat emacs-root-path "templates/") "Path for template")))
+  (concat emacs-root-path "templates/") "Path for template")
 
 (setq default-directory emacs-root-path)
 
+;; add load path
+(add-to-list 'load-path settings-path-r)
+(add-to-list 'load-path plugins-path-r)
+
+(require 'theme-settings)
 
 
 (provide 'emacs-dealM)
---
+++ settings/theme-settings.el
@@ -0,0 +1,19 @@
+;; -*- Emacs-Lisp -*-
+;; Last modified: <2012-07-26 08:38:50 Thursday by richard>
+
+;; Copyright (C) 2012 Richard Wong
+
+;; Author: Richard Wong
+;; Email: chao787@gmail.com
+
+;; Version: 0.1
+;; PUBLIC LICENSE: FreeBSD
+(add-to-list 'custom-theme-load-path (concat plugins-path-r "emacs-color-theme-solarized/"))
+
+(load-theme 'solarized-dark t)
+
+
+
+(provide 'theme-settings)
+;; theme-settings ends here.
+;;;
