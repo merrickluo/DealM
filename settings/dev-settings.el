ae930c78303b18a19f382762a6718133550eb9c5
Richard Wong <chao787@qq.com>
2012-10-29 11:30:44 +0800

    Add go-mode and make go-mode autoloaded.

--- settings/dev-settings.el
+++ settings/dev-settings.el
@@ -1,5 +1,5 @@
 ;; -*- Emacs-Lisp -*-
-;; Last modified: <2012-09-04 17:29:34 Tuesday by richard>
+;; Last modified: <2012-10-29 11:27:07 Monday by richard>
 
 ;; Copyright (C) 2012 Richard Wong
 
@@ -33,6 +33,32 @@
   (font-lock-add-keywords
    nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|HACK\\|REFACTOR\\|NOCOMMIT\\|WARN\\(ING\\)?\\)"
           1 font-lock-warning-face t))))
+
+
+;; Go-mode settings. (go programming language)
+;; ------------------------------------------------------------------
+(autoload 'go-mode "go-mode" "\
+Major mode for editing Go source text.
+
+This provides basic syntax highlighting for keywords, built-ins,
+functions, and some types.  It also provides indentation that is
+\(almost) identical to gofmt.
+
+\(fn)" t nil)
+
+(autoload 'gofmt "go-mode" "\
+Pipe the current buffer through the external tool `gofmt`.
+Replace the current buffer on success; display errors on failure.
+
+\(fn)" t nil)
+
+(autoload 'gofmt-before-save "go-mode" "\
+Add this to .emacs to run gofmt on the current buffer when saving:
+ (add-hook 'before-save-hook #'gofmt-before-save)
+
+\(fn)" t nil)
+
+
 
 
 ;; Find-things-fast settings.
