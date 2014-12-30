b260b8ec1393e255bb0659b284f91975ef70a56a
Richard Wong <chao787@gmail.com>
2014-12-30 15:21:48 +0800

    Support python virturlenv per buffer

--- settings/python-settings.el
+++ settings/python-settings.el
@@ -1,5 +1,5 @@
 ;; -*- Emacs-Lisp -*-
-;; Last modified: <2014-11-11 14:25:55 Tuesday by wongrichard>
+;; Last modified: <2014-12-30 15:21:39 Tuesday by wongrichard>
 
 ;; Copyright (C) 2012 Richard Wong
 
@@ -11,9 +11,13 @@
 
 (autoload 'python-mode "python" "python Editing Mode" t)
 
-
 (setq jedi:complete-on-dot t
-      jedi:key-goto-definition (kbd "C-c g"))
+      jedi:tooltip-method nil
+      jedi:key-goto-definition (kbd "C-c g")
+      jedi:environment-root "jedi"
+      jedi:environment-virtualenv '("--python" "/usr/local/bin/python3")
+      jedi:server-command (list "python3" (concat plugins-path-r "emacs-jedi/jediepcserver.py")
+                                "--virtual-env" "../.env/\"${PWD##*/}\"/"))
 
 (add-to-list 'load-path (concat plugins-path-r "emacs-jedi/"))
 (add-to-list 'load-path (concat plugins-path-r "emacs-python-environment/"))
@@ -22,10 +26,12 @@
 (autoload 'jedi:ac-setup "jedi" "" t)
 (autoload 'jedi-mode "jedi" "" t)
 
-(setq py-python-command-args '( "--colors" "Linux")
-      py-smart-indentation t
-      py-shell-name "python2"
-      py-indent-offset 4)
+(setq py-smart-indentation t
+      py-shell-name "python3"
+      python-shell-interpreter py-shell-name
+      python-default-indent-offset 4
+      python-indent-offset 4
+      py-indent-offset python-indent-offset)
 
 (autoload 'nosetests-all        "nose" "" t)
 (autoload 'nosetests-module     "nose" "" t)
@@ -35,26 +41,32 @@
 (autoload 'nosetests-pdb-one    "nose" "" t)
 
 (defun python-dev-settings()
-    (jedi:ac-setup)
-    (jedi-mode 1)
-    (local-set-key "\C-x\M-w"  'py-copy-clause)
-    (local-set-key "\C-x\C-p"  'py-up-clause)
-    (local-set-key "\C-x\C-n"  'py-down-clause)
-    (local-set-key "\C-cta"  'nosetests-all)
-    (local-set-key "\C-ctm"  'nosetests-module) ;; C-c m conflicts w/ pylint
-    (local-set-key "\C-ct."  'nosetests-one)
-    (local-set-key "\C-ctt"  'nosetests-one)
-    (local-set-key "\C-cpa" 'nosetests-pdb-all)
-    (local-set-key "\C-cpm" 'nosetests-pdb-module)
-    (local-set-key "\C-cp." 'nosetests-pdb-one))
+
+  ;; set environment conform my python3 venv rules
+  ;; alias ss='source ../.env/"${PWD##*/}"/bin/activate'
+  ;; alias pss='python3 -m venv ../.env/"${PWD##*/}"'
+  ;; alias rmss='rm -rf ../.env/"${PWD##*/}"'
+  (when (projectile-project-p)
+    (set (make-local-variable 'jedi:server-command)
+         (list "python3" (concat plugins-path-r "emacs-jedi/jediepcserver.py")
+               "--virtual-env" (concat (projectile-project-root) "../.env/" (projectile-project-name))))
+    )
+  (jedi:ac-setup)
+  (jedi-mode 1)
+  (local-set-key "\C-x\M-j" 'run-python)
+  (local-set-key "\C-x\M-w" 'py-copy-clause)
+  (local-set-key "\C-x\C-p" 'py-up-clause)
+  (local-set-key "\C-x\C-n" 'py-down-clause)
+  (local-set-key "\C-cta"   'nosetests-all)
+  (local-set-key "\C-ctm"   'nosetests-module) ;; C-c m conflicts w/ pylint
+  (local-set-key "\C-ct."   'nosetests-one)
+  (local-set-key "\C-ctt"   'nosetests-one)
+  (local-set-key "\C-cpa"   'nosetests-pdb-all)
+  (local-set-key "\C-cpm"   'nosetests-pdb-module)
+  (local-set-key "\C-cp."   'nosetests-pdb-one))
+
 (add-hook 'python-mode-hook 'python-dev-settings)
-(when (string= system-type "darwin")
-;;; ENV path correction for (Mac os x)
-  (setenv "PYTHONPATH"
-          (concat
-           (getenv "PYTHONPATH") path-separator
-           "/Library/Python/2.7/site-packages/" path-separator
-           "/usr/local/lib/python2.7/site-packages/")))
+
 
 (provide 'python-settings)
 ;; python-settings ends here.
