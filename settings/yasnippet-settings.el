7d3937989c364105569aa290ed556e392f0e7012
Richard Wong <wangrunchao@huobi.com>
2016-09-18 11:02:41 +0800

    upgrade yasnippet to 0.10.0

--- settings/yasnippet-settings.el
+++ settings/yasnippet-settings.el
@@ -1,7 +1,7 @@
 ;; -*- Emacs-Lisp -*-
 ;; Copyright (C) 2012 Richard Wong
 
-;; Last modified: <2013-03-22 09:01:57 Friday by richard>
+;; Last modified: <2016-09-18 10:59:36 Sunday by richard>
 
 ;; Author: Richard Wong
 ;; Email: chao787@gmail.com
@@ -10,16 +10,62 @@
 ;; PUBLIC LICENSE: GPLv3
 
 (add-to-list 'load-path (concat plugins-path-r "yasnippet/"))
-(require 'yasnippet)
 
-(setq yas-snippet-dirs (concat emacs-root-path "snippets/"))
+(use-package
+  yasnippet
+  :commands (yas-global-mode)
+  :init
+  (setq yas-snippet-dirs (concat emacs-root-path "snippets/"))
+  :bind
+  (("\t"      . yas-next-field-or-maybe-expand)
+   ("M-k"     . yas-prev-field)
+   :map yas-minor-mode-map
+   ("C-c C-f" . yas-find-snippets))
+  :config
+  (yas-load-directory yas-snippet-dirs)
+  (defun yas-find-snippets (&optional same-window)
+    "Find snippet file in guessed current mode's directory.
 
-(yas-load-directory yas-snippet-dirs)
+Calls `find-file' interactively in the guessed directory.
+
+With prefix arg SAME-WINDOW opens the buffer in the same window.
+
+Because snippets can be loaded from many different locations,
+this has to guess the correct directory using
+`yas--guess-snippet-directories', which returns a list of
+options.
+
+If any one of these exists, it is taken and `find-file' is called
+there, otherwise, proposes to create the first option returned by
+-`yas--guess-snippet-directories'."
+    (interactive "P")
+    (let* ((guessed-directories (yas--guess-snippet-directories))
+           (chosen)
+           (buffer))
+      (setq chosen (yas--make-directory-maybe (first guessed-directories) " main"))
+      (unless chosen
+        (if (y-or-n-p (format "Continue guessing for other active tables %s? "
+                              (mapcar #'(lambda (table-and-dirs)
+                                          (yas--table-name (car table-and-dirs)))
+                                      (rest guessed-directories))))
+            (setq chosen (some #'yas--make-directory-maybe
+                               (rest guessed-directories)))))
+      (unless chosen
+        (when (y-or-n-p "Having trouble... go to snippet root dir? ")
+          (setq chosen (first (yas/snippet-dirs)))))
+      (if chosen
+          (let ((default-directory chosen))
+            (setq buffer (call-interactively (if same-window
+                                                 'find-file
+                                               'find-file-other-window)))
+            (when buffer
+              (save-excursion
+                (set-buffer buffer)
+                (when (eq major-mode 'fundamental-mode)
+                  (snippet-mode)))))
+        (message "Could not guess snippet dir!")))))
+
 (yas-global-mode 1)
-
-(global-set-key (kbd "\t")                     'yas-next-field-or-maybe-expand)
-(global-set-key (kbd "M-k")                    'yas-prev-field)
-(define-key yas-minor-mode-map (kbd "C-c C-f") 'yas-find-snippets)
 
 (provide 'yasnippet-settings)
 ;; yasnippet-settings ends here.
