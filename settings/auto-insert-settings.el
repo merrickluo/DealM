17d618b1f35b2656a1d3d94bf35ba0c7a7744783
Richard Wong <wangrunchao@huobi.com>
2016-09-23 00:04:27 +0800

    remove useless lazy load for auto-insert-settings

--- settings/auto-insert-settings.el
+++ settings/auto-insert-settings.el
@@ -1,73 +1,71 @@
 ;; -*- Emacs-Lisp -*-
-;; Last modified: <2013-12-20 18:24:36 Friday by wongrichard>
+;; Last modified: <2016-09-23 00:03:59 Friday by richard>
 
 ;; Copyright (C) 2012 Richard Wong
 
 ;; Author: Richard Wong
 ;; Email: chao787@gmail.com
 
-;; Version: 0.2
+;; Version: 0.3
 ;; PUBLIC LICENSE: GPLv3
+;; The auto-insert-settings do *NOT* need use-package style writting
+
+(setq auto-insert-query nil
+      auto-insert-directory template-path-r)
+
+(defun insert-snippet (snippet)
+  "Insert snippet SNIPPET."
+  (interactive "s")
+  (insert snippet)
+  (yas-expand))
+
+(defun insert-headx-snippet ()
+  "Insert headx snippet."
+  (insert-snippet "headx"))
+
+(defun insert-abbrev (abbrev-name)
+  "Insert abbrev ABBREV-NAME"
+  (interactive "s")
+  (insert abbrev-name)
+  (expand-abbrev))
+
+;; C/C++ head file.
+(define-auto-insert "\\.\\([Hh]\\|hh\\|hxx\\|hpp\\)$"
+  '(lambda()
+     (insert-snippet "headxh")))
+
+;; Add plan Snippet.
+(define-auto-insert "\\.\\(todo\\|do\\|plan\\)$"
+  '(lambda()
+     (insert-snippet "headxplan")))
+
+;; Flex snippet.
+(define-auto-insert "\\.l[l]?$"
+  '(lambda()
+     (insert-snippet "headxflex")))
+
+(mapc
+ (lambda (suffix)
+   (define-auto-insert (concat "\\." suffix "$") 'insert-headx-snippet))
+ '("[Cc][Pp][Pp]"      ;; C++
+   "[Cc][Xx][Xx]"      ;; C++
+   "[Tt]?[Cc][Cc]"     ;; C++
+   "i"                 ;; C++
+   "js"                ;; js
+   "CMakeLists.txt"    ;; js
+   "makefile"          ;; makefile
+   "headx"             ;; snippet
+   "y[s]?"             ;; yassnippet
+   "yy"                ;; bison
+   "[Yy][Aa]?[Mm][Ll]" ;; YAML
+   "el"                ;; elisp
+   "sh"                ;; shell
+   "org"               ;; org
+   "pl"
+   "py"                ;; python
+   "htm\\(l\\)?"))
 
 (auto-insert-mode 1)
-
-(defun auto-insert-settings ()
-  "Settings for `auto-insert'."
-  (setq auto-insert-query nil)
-  (setq auto-insert-directory template-path-r)
-  ;; CMakeLists.txt (CMake file)
-  (define-auto-insert "CMakeLists.txt$"                 '(lambda()
-                                                           (insert-snippet "headx")))
-  ;; C/C++ head file.
-  (define-auto-insert "\\.\\([Hh]\\|hh\\|hxx\\|hpp\\)$" '(lambda()
-                                                           (insert-snippet "headxh")))
-  ;; Add plan Snippet.
-  (define-auto-insert "\\.\\(todo\\|do\\|plan\\)$"      '(lambda()
-                                                           (insert-snippet "headxplan")))
-  ;; Add js Snippet.
-  (define-auto-insert "\\.\\(js\\)$"                    '(lambda()
-                                                           (insert-snippet "headx")))
-  ;; Flex snippet.
-  (define-auto-insert "\\.l[l]?$"                       '(lambda()
-                                                           (insert-snippet "headxflex")))
-
-  (defun insert-headx-snippet ()
-    "Insert headx snippet."
-    (insert-snippet "headx"))
-
-  (defun insert-abbrev (abbrev-name)
-    "Insert abbrev ABBREV-NAME"
-    (interactive "s")
-    (insert abbrev-name)
-    (expand-abbrev))
-
-  (defun insert-snippet (snippet)
-    "Insert snippet SNIPPET."
-    (interactive "s")
-    (insert snippet)
-    (yas-expand))
-
-  (mapc
-   (lambda (suffix)
-     (define-auto-insert (concat "\\." suffix "$") 'insert-headx-snippet))
-   '("[Cc][Pp][Pp]"                ;; C++
-     "[Cc][Xx][Xx]"                ;; C++
-     "[Tt]?[Cc][Cc]"               ;; C++
-     "i"                           ;; C++
-     "makefile"                    ;; makefile
-     "headx"                       ;; snippet
-     "y[s]?"                       ;; yassnippet
-     "yy"                          ;; bison
-     "[Yy][Aa]?[Mm][Ll]"           ;; YAML
-     "el"                          ;; elisp
-     "sh"                          ;; shell
-     "org"                         ;; org
-     "pl"
-     "py"                          ;; python
-     "htm\\(l\\)?")))              ;; HTML
-
-(eval-after-load "autoinsert"
-  `(auto-insert-settings))
 
 (provide 'auto-insert-settings)
 ;; auto-insert-settings ends here.
