56906805ec5b086f586c1c0d157883c7400d2cd3
Richard Wong <wangrunchao@huobi.com>
2016-09-23 11:48:40 +0800

    adapt dired{*} with use-package

--- settings/dired-settings.el
+++ settings/dired-settings.el
@@ -1,27 +1,36 @@
 ;; -*- Emacs-Lisp -*-
-;; Last modified: <2014-11-12 11:48:58 Wednesday by wongrichard>
+;; Last modified: <2016-09-23 11:43:57 Friday by richard>
 
 ;; Copyright (C) 2012-2013 Richard Wong
 
 ;; Author: Richard Wong
 ;; Email: chao787@gmail.com
 
-;; Version: 0.1
+;; Version: 0.2
 ;; PUBLIC LICENSE: GPLv3
 
-(require 'dired+)
-(define-key dired-mode-map "\C-o"    'dired-display-file)         ; `C-o'
-(define-key dired-mode-map "\M-b"    'backward-word)              ; `M-b'
+(use-package dired-x
+  :defer t
+  :commands
+  (dired-omit-mode)
+  :init     ; before code load
+  (add-hook 'dired-mode-hook
+            #'(lambda ()
+                (dired-omit-mode t))))
 
-(add-hook 'dired-mode-hook '(lambda ()
-                              (require 'dired-x)
-                              (dired-omit-mode t)))
+(use-package dired
+  :defer t
+  :init
+  (setq dired-dwim-target t)
+  :bind (:map dired-mode-map
+         ("C-o" . dired-display-file)
+         ("M-b" . backward-word)))
 
-(setq dired-dwim-target t)
-
-(autoload 'openwith-mode "openwith" t)
-
-(defun dired-key-bindings ()
+(use-package dired+
+  :after (dired)
+  :defer t
+  :config   ; execute code after a package is loaded
+  (autoload 'openwith-mode "openwith" t)
   (openwith-mode t)
 
   (if (string= system-type "darwin")
@@ -38,9 +47,7 @@
   ;; From color-moccur
   (define-key dired-mode-map "O" 'dired-do-moccur)
   (defvar dired-sort-map (make-sparse-keymap))
-
   (define-key dired-mode-map "s" dired-sort-map)
-
   (define-key dired-sort-map "s" (lambda () "sort by Size" (interactive) (dired-sort-other (concat dired-listing-switches " -S"))))
   (define-key dired-sort-map "x" (lambda () "sort by eXtension" (interactive) (dired-sort-other (concat dired-listing-switches " -X"))))
   (define-key dired-sort-map "t" (lambda () "sort by Time" (interactive) (dired-sort-other (concat dired-listing-switches " -t"))))
@@ -49,8 +56,7 @@
   (define-key dired-sort-map "h" (lambda () "sort help" (interactive) (message "s Size; x eXtension; t Time; n Name")))
   (define-key dired-sort-map "/" (lambda () "sort help" (interactive) (message "s Size; x eXtension; t Time; n Name"))))
 
-(eval-after-load "dired"
-  '(dired-key-bindings))
+
 
 (provide 'dired-settings)
 ;; dired-settings ends here.
