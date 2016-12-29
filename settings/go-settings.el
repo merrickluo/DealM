aefc67c5adcb2bc9913fcd85880fd768502bf5d7
A.I <merrick@luois.me>
2016-12-29 18:13:15 +0800

    protobuf mode

--- settings/go-settings.el
+++ settings/go-settings.el
@@ -1,5 +1,5 @@
 ;; -*- Emacs-Lisp -*-
-;; Last modified: <2016-12-27 18:17:51 Tuesday by merrick>
+;; Last modified: <2016-12-29 17:49:24 Thursday by merrick>
 
 ;; Copyright (C) 2016 Richard Wong
 
@@ -12,11 +12,16 @@
 (add-to-list 'load-path (concat plugins-path-r "go-mode"))
 (add-to-list 'load-path (concat plugins-path-r "go-eldoc"))
 
+(defun go-set-shortcut ()
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-*") 'pop-tag-mark))
+
 (use-package go-mode
   :after (go-eldoc go-autocomplete)
   :commands (gofmt gofmt-before-save)
   :mode "\\.go\\'"
   :init
+  (add-hook 'go-mode-hook 'go-set-shortcut)
   (add-hook 'before-save-hook 'gofmt-before-save))
 
 (use-package go-eldoc
@@ -27,6 +32,9 @@
   :init
   (add-hook 'go-mode-hook 'auto-complete-mode)
   (add-hook 'go-mode-hook 'yas-minor-mode))
+
+(use-package protobuf-mode
+  :mode "\\.proto\\'")
 
 (provide 'go-settings)
 ;; go-settings ends here.
